2a21ad82724ebc625164d622b5d8e98973b89110
Villy Siu <villysiu@gmail.com>
2017-10-30 00:05:50 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,6 +15,6 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(q)
-    self.all.select { |s| s.name.include? 'r' }
+    self.all.select { |s| s.name.include? 'rob' }
   end
 end
